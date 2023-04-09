51aa11a9242038eccf7dc4a4d5ee15824824cae6
Roman Artemenko <romma.art@gmail.com>
2023-04-10 00:23:40 +0300

    [Multithreading] Task5

--- Multithreading/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ Multithreading/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,18 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        const int ElementsCount = 10;
+        static readonly object lockObject = new object();
+        static List<int> sharedCollection = new List<int>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +24,37 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            Task.Run(AddElements);
+            Task.Run(PrintElements);
 
             Console.ReadLine();
+        }
+
+        static void AddElements()
+        {
+            for (int i = 0; i < ElementsCount; i++)
+            {
+                lock (lockObject)
+                {
+                    sharedCollection.Add(i);
+                    Console.WriteLine($"Added element {i} to the collection.");
+                    Monitor.Pulse(lockObject);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void PrintElements()
+        {
+            while (sharedCollection.Count < ElementsCount)
+            {
+                lock (lockObject)
+                {
+                    Monitor.Wait(lockObject);
+                    Console.WriteLine("Elements in the collection:");
+                    Console.WriteLine(string.Join(", ", sharedCollection) + "\n");
+                }
+            }
         }
     }
 }

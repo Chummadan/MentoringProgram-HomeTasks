fc9140fbde344722f9adddff1f7b668c7662c66f
Roman Artemenko <romma.art@gmail.com>
2023-04-17 21:14:26 +0300

    [Multithreading] Optional task draft sln

---
+++ Multithreading/02.optional/ThreadBasedEnterpriseChat/Server/Announcer.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Announcer
+    {
+        public static void Broadcast(string clientName, Dictionary<string, TcpClient> clientsList)
+        => BroadcastMessage(GetMessageBytes(MessageGenerator.GenerateWelcomeMessage(clientName)), clientsList);
+
+        public static void Broadcast(string clientName, string message, Dictionary<string, TcpClient> clientsList)
+            => BroadcastMessage(GetMessageBytes(MessageGenerator.GenerateUserMessage(clientName, message)), clientsList);
+
+        static byte[] GetMessageBytes(string message)
+            => Encoding.ASCII.GetBytes(message + Environment.NewLine);
+
+        static void BroadcastMessage(byte[] buffer, Dictionary<string, TcpClient> clientsList)
+        {
+            foreach (TcpClient client in clientsList.Values)
+                client.GetStream().Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ Multithreading/02.optional/ThreadBasedEnterpriseChat/Server/MessageStore.cs
@@ -0,0 +1,25 @@
+﻿namespace Server
+{
+    class MessageStore
+    {
+        readonly int maxMessages;
+        Queue<string> messageQueue;
+
+        public MessageStore(int maxMessages)
+        {
+            this.maxMessages = maxMessages;
+            messageQueue = new();
+        }
+
+        public void AddMessage(string message)
+        {
+            messageQueue.Enqueue(message);
+
+            while (messageQueue.Count > maxMessages)
+                messageQueue.Dequeue();
+        }
+
+        public List<string> GetLastMessages()
+            => messageQueue.ToList();
+    }
+}

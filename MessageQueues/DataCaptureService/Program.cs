2e9136871d49451805b4128b5c372204b8f6442e
Roman Artemenko <romma.art@gmail.com>
2023-04-27 20:52:08 +0300

    [MessageQueues] Complete Tasks1

---
+++ MessageQueues/DataCaptureService/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+
+namespace DataCaptureService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("pdfExchange", ExchangeType.Direct, true);
+
+            var watchFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestFiles");
+            using var watcher = new FileSystemWatcher(watchFolder, "*.pdf");
+            watcher.EnableRaisingEvents = true;
+            watcher.Created += (sender, e) =>
+            {
+                try
+                {
+                    //var body = File.ReadAllBytes(e.FullPath);
+                    //channel.BasicPublish(exchange: "pdfExchange", routingKey: "pdf.created", null, body);
+                    var readTask = Task.Factory.StartNew(() =>
+                    {
+                        using var stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+                        byte[] buffer = new byte[stream.Length];
+                        stream.Read(buffer);
+
+                        return buffer;
+                    });
+
+                    readTask.ContinueWith(task =>
+                    {
+                        channel.BasicPublish(exchange: "pdfExchange", routingKey: "pdf.created", null, task.Result);
+                        Console.WriteLine("Sent document {0} to processing service", e.Name);
+                    });
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error processing document {0}: {1}", e.Name, ex.Message);
+                }
+            };
+
+            Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ MessageQueues/ProcessingService/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ProcessingService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare("pdfQueue", true, false, false);
+            channel.QueueBind("pdfQueue", "pdfExchange", "pdf.created");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var fileName = Guid.NewGuid().ToString() + ".pdf";
+                var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Results", fileName);
+                File.WriteAllBytes(filePath, eventArgs.Body.ToArray());
+
+                Console.WriteLine("Received and saved document {0} from data capture service", fileName);
+            };
+            channel.BasicConsume("pdfQueue", true, consumer);
+
+            Console.ReadLine();
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
